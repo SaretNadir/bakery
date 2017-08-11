9430cedd6cf7a8af78e81b6ef70d940c87bb5bb1
SaretNadir <saretnadir@gmail.com>
2017-08-11 19:27:53 -0400

    added category form

--- app/components/CreateCategories.js
+++ app/components/CreateCategories.js
@@ -2,17 +2,38 @@ import React from "react";
 
 
 class CreateCategories extends React.Component {
+/*     constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            code: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    } */
+   
+
     render() {
-
-
         return (
-            <button type="button" className="btn btn-primary">
-                <i className="fa fa-superpowers" aria-hidden="true"></i>
-                Primaries
-                <i className="glyphicon glyphicon-heart"></i>
-            </button>
-
-        )
+            <div className="container-fluid">
+                <div className="row">                    
+                        <form>
+                            <h1>Add Category</h1>
+                            <hr />
+                            <div className="form-group">
+                                <label htmlFor="categoryName">Category Name:</label>
+                                <input type="text" className="form-control" id="categoryName" placeholder="Name"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="categoryCode">Category Code:</label>
+                                <input type="text" className="form-control" id="categoryCode" placeholder="Code"/>
+                            </div>
+                            <input type="submit" className="btn btn-default col-xs-offset-9 col-xs-3" value="Submit" />
+                        </form>
+                    </div>
+                </div>
+        );
     }
 }
 
