2077e8134c090b69b010746edbbddb1b19df6f22
SaretNadir <saretnadir@gmail.com>
2017-08-25 17:38:20 -0400

    css products view, added navbar to app.js

--- app/app.js
+++ app/app.js
@@ -14,14 +14,55 @@ import Admin from "./components/Admin";
 
 ReactDOM.render(
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/categories" component={Categories} />
-            <Route exact path="/categories/create" component={CreateCategory} />
-            <Route exact path="/products" component={Products} />
-            <Route exact path="/products/create" component={CreateProduct} />
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/admin" component={Admin} />
-        </Switch>
+        <div>
+            <nav className="navbar navbar-fixed-top navbar-inverse">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#display-menu" aria-expanded="false">
+                            <span className="sr-only"></span>
+                            <span className="icon-bar top-bar"></span>
+                            <span className="icon-bar middle-bar"></span>
+                            <span className="icon-bar bottom-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="/" alt="QueQueres">
+                            <img src="https://kloudless.com/wp-content/uploads/2016/02/notification.png" alt="" />
+                        </a>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="display-menu">
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a href="/products">Products</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Contact</a></li>
+                            <li><a href=""><i className="fa fa-shopping-bag" aria-hidden="true"></i></a></li>
+                            <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-user-o" aria-hidden="true"></i><span className="caret"></span></a>
+                                <ul className="dropdown-menu">
+                                    <li><a href="">Your Account</a></li>
+                                    <li><a href="">Your Orders</a></li>
+                                    <li role="separator" className="divider"></li>
+                                    <li><a href="">Sign in / Sign out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <Switch>
+                <Route exact path="/categories" component={Categories} />
+                <Route exact path="/categories/create" component={CreateCategory} />
+                <Route exact path="/products" component={Products} />
+                <Route exact path="/products/create" component={CreateProduct} />
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/admin" component={Admin} />
+            </Switch>
+
+            <footer>
+                <div className="container">
+                    <p>Copyright &copy; QueQueres 2017</p>
+                </div>
+            </footer>
+        </div>
     </BrowserRouter>,
     document.getElementById("app")
 );
--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -12,38 +12,6 @@ class HomePage extends React.Component {
     render() {
         return (
             <div id="HomePage">
-                <nav className="navbar navbar-fixed-top navbar-inverse">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#display-menu" aria-expanded="false">
-                                <span className="sr-only"></span>
-                                <span className="icon-bar top-bar"></span>
-                                <span className="icon-bar middle-bar"></span>
-                                <span className="icon-bar bottom-bar"></span>
-                            </button>
-                            <a className="navbar-brand" href="/" alt="QueQueres">
-                                <img src="https://kloudless.com/wp-content/uploads/2016/02/notification.png" alt="" />
-                            </a>
-                        </div>
-
-                        <div className="collapse navbar-collapse" id="display-menu">
-                            <ul className="nav navbar-nav navbar-right">
-                                <li><a href="/products">Products</a></li>
-                                <li><a href="">About</a></li>
-                                <li><a href="">Contact</a></li>
-                                <li className="dropdown"><a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customer Name <span className="caret"></span></a>
-                                    <ul className="dropdown-menu">
-                                        <li><a href="">Your Account</a></li>
-                                        <li><a href="">Your Orders</a></li>
-                                        <li role="separator" className="divider"></li>
-                                        <li><a href="">Sign in / Sign out</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
                 <div id="my-carousel" className="carousel slide" data-ride="carousel" data-interval="4000">
                     <ol className="carousel-indicators">
                         <li data-target="#my-carousel" data-slide-to="0" className="active"></li>
@@ -114,12 +82,6 @@ class HomePage extends React.Component {
                         </div>
                     </div>
                 </div>
-
-                <footer>
-                    <div className="container">
-                        <p>Copyright &copy; QueQueres 2017</p>
-                    </div>
-                </footer>
             </div>
         )
     }
